49a3d98b2ac5842fbaceed8af2d0959366b7ead6
JuanGrajales <inbox1.juan+github@gmail.com>
2021-02-22 16:17:58 -0500

    attemping to fix login bug due to cors error

--- app.js
+++ app.js
@@ -12,20 +12,24 @@ const path = require("path");
 const cors = require("cors");
 const session = require("express-session");
 const passport = require("passport");
-require("./config/passport"); 
+require("./config/passport");
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-.then(x => {
-  console.log(
-    `Connected to Mongo! Database name: "${x.connections[0].name}"`
-  );
-})
-.catch(err => {
-  console.error("Error connecting to mongo", err);j
-});
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+    j;
+  });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
@@ -34,13 +38,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+let whitelist = ["http://localhost:3000", "addDeployedURL"];
+let corsOptions = {
+  credentials: true,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// test this cors configuration to see if login bug is fixed in deployment
+// app.use(
+//   cors(corsOptions)
+// );
+
 app.use(
   cors({
     credentials: true,
-    origin: function(origin, callback) {
-      return callback(null, true)
-    }
-    
+    origin: function (origin, callback) {
+      return callback(null, true);
+    },
+  })
+);
+
+app.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      return callback(null, true);
+    },
   })
   // cors()
 );
@@ -50,9 +79,9 @@ app.use(
 app.use(
   session({
     secret: process.env.SECRET,
-    resave: false, 
+    resave: false,
     saveUninitialized: true,
-    cookie: {maxAge: 1000 * 60 *60}
+    cookie: { maxAge: 1000 * 60 * 60 },
   })
 );
 
@@ -63,10 +92,8 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-app.use('/api', require('./routes/task-routes'));
-app.use('/api', require('./routes/auth-routes'))
-
-
+app.use("/api", require("./routes/task-routes"));
+app.use("/api", require("./routes/auth-routes"));
 
 // app.get('*', (req,res) => {
 //   res.sendFile(path.join(__dirname, '../public/index.html'))
